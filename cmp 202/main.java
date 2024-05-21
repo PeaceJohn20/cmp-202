0d60e68bf39585183369fb9ce35a6f5feea5344d
PeaceJohn20 <159129872+PeaceJohn20@users.noreply.github.com>
2024-05-21 15:15:51 +0100

    Add files via upload

---
+++ cmp 202/main.java
@@ -0,0 +1,16 @@
+Public class Main {
+    public static void main(string[]args){
+        CMP202 printUser = new CMP202();
+        printUser.printuserdetails();
+        Person student = new 
+        Person ("peace", "john","12345", "BHU/23/04/09/0086","123");
+        Person staff = new Person ("Staff Name", "Bingham", "234234", null,445);
+        System.out.printIn(staff);
+
+        //
+        System.out.printIn(person.getName())
+        //
+        system.out.printIn(person.setPassword("146132"));;
+        //person.password = "12345";
+    }
+}
